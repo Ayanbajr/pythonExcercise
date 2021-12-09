63005444d65f1285c4a2c391bbad17efd5499098
Ayanbajr <68592003+Ayanbajr@users.noreply.github.com>
2021-12-09 19:53:38 +0530

    Add files via upload

---
+++ python_CC_2021.py
@@ -0,0 +1,79 @@
+####d = (21,"yan", 23) tupple
+##
+####d = [21, 'ayan', 23]  #List
+##
+##d = {21, 'ayan', 23} #set
+##
+##d = {
+##    'ayan' : 'path in which sun walks in day time'   # Dict 
+##    }
+##
+##print(type(d))
+##
+##item = ['yana',26,1995]
+##item[0] = 'ayan'
+##print(item)
+
+# 1 can not edit item in tuple but 1 can edit itms in list
+
+#Conditional
+##var = int(input('Give me one number: ' ))
+##if (var > 4):
+##    print('This no. is bigger than 4')
+##elif (var == 2):
+##    print('This no. is equal to 2')
+##else:
+##    print('This no. is not greater than 4')
+
+# For Loop
+##for i in range (0,101):
+##    print(i)
+
+# While Loop
+##
+##i = 0
+##while (i<101):
+##    print(i)
+##    i = i+1
+
+# Python Function
+
+def average(a, b):
+    avg = (a+b)/2
+    return avg
+print(average(10,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
